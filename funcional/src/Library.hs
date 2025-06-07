93d4dfc12254d4f7807cbe78e744ffbba3cea608
lautarocatriel <lauchacatriel@gmail.com>
2025-06-07 20:28:57 -0300

    ejercicios de composicion y aplicacion parcial

--- funcional/src/Library.hs
+++ funcional/src/Library.hs
@@ -74,7 +74,8 @@ diasLocos :: Number-> Number -> Number -> Bool
 diasLocos medicion1 medicion2 medicion3 = dispersion medicion1 medicion2 medicion3 > 100
 
 diasNormales :: Number-> Number -> Number -> Bool
-diasNormales medicion1 medicion2 medicion3 = not (diasParejos medicion1 medicion2 medicion3) && not (diasLocos medicion1 medicion2 medicion3)
+diasNormales medicion1 medicion2 medicion3 = ((diasParejos medicion1 medicion2 medicion3 /=) .not) (diasLocos medicion1 medicion2 medicion3)
+
 
 -- En una plantación de pinos, de cada árbol se conoce la altura expresada en cm. El peso de un pino se puede calcular a partir de la altura así: 3 kg x cm hasta 3 metros, 2 kg x cm arriba de los 3 metros. P.ej. 2 metros ⇒  600 kg, 5 metros ⇒  1300 kg. 
 -- Los pinos se usan para llevarlos a una fábrica de muebles, a la que le sirven árboles de entre 400 y 1000 kilos, un pino fuera de este rango no le sirve a la fábrica. Para esta situación: 
@@ -92,3 +93,81 @@ esPesoUtil peso = peso <= 1000 && peso >=400
 
 sirvePino:: Number -> Bool
 sirvePino = esPesoUtil . pesoPino
+
+
+-- Este ejercicio alguna vez se planteó como un Desafío Café con Leche: Implementar la función esCuadradoPerfecto/1, sin hacer operaciones con punto flotante. Ayuda: les va a venir bien una función auxiliar, tal vez de dos parámetros. Pensar que el primer cuadrado perfecto es 0, para llegar al 2do (1) sumo 1, para llegar al 3ro (4) sumo 3, para llegar al siguiente (9) sumo 5, después sumo 7, 9, 11 etc.. También algo de recursividad van a tener que usar. 
+
+--APLICACION PARCIAL
+
+-- Definir una función siguiente, que al invocarla con un número cualquiera me devuelve el resultado de sumar a ese número el 1. 
+
+siguiente :: Number -> Number
+siguiente = (+1)
+
+-- Definir la función mitad que al invocarla con un número cualquiera me devuelve la mitad de dicho número, ej: 
+mitad :: Number -> Number
+mitad = (/2)
+
+-- Definir una función inversa, que invocando a la función con un número cualquiera me devuelva su inversa. 
+-- Main> inversa 4
+-- 0.25
+-- Main> inversa 0.5
+-- 2.0
+
+inversa :: Number -> Number
+inversa = (1/)
+
+-- Definir una función triple, que invocando a la función con un número cualquiera me devuelva el triple del mismo.
+
+triple :: Number -> Number
+triple = (*3)
+
+-- Definir una función esNumeroPositivo, que invocando a la función con un número cualquiera me devuelva true si el número es positivo y false en caso contrario. 
+-- Main> esNumeroPositivo (-5)
+-- False
+-- Main> esNumeroPositivo 0.99
+-- True 
+
+esNumeroPositivo :: Number -> Bool
+esNumeroPositivo = (>0)
+
+--COMPOSICION
+
+-- Resolver la función del ejercicio 2 de la guía anterior esMultiploDe/2, utilizando aplicación parcial y composición.
+
+esMultiploDeC :: Number -> Number -> Bool
+esMultiploDeC numero otro = ((==0) . mod otro) numero
+
+-- Resolver la función del ejercicio 5 de la guía anterior esBisiesto/1, utilizando aplicación parcial y composición.
+
+-- esBisiesto :: Number -> Bool
+-- esBisiesto numero = esMultiploDe 400 numero || esMultiploDe 4 numero && (not . esMultiploDe 100) numero
+
+esBisiestoC :: Number -> Bool
+esBisiestoC numero = esMultiploDe 400 numero || ((==esMultiploDe 100 numero) . not . esMultiploDe 4) numero
+
+-- Resolver la función inversaRaizCuadrada/1, que da un número n devolver la inversa su raíz cuadrada. 
+-- Main> inversaRaizCuadrada 4 
+-- 0.5 
+-- Nota: Resolverlo utilizando la función inversa Ej. 2.3, sqrt y composición.
+
+inversaRaizCuadrada :: Number -> Number
+inversaRaizCuadrada = inversa . sqrt
+
+-- Definir una función incrementMCuadradoN, que invocándola con 2 números m y n, incrementa un valor m al cuadrado de n por Ej: 
+-- Main> incrementMCuadradoN 3 2 
+-- 11 
+-- Incrementa 2 al cuadrado de 3, da como resultado 11. Nota: Resolverlo utilizando aplicación parcial y composición. 
+
+incrementMcuadradoN :: Number -> Number -> Number
+incrementMcuadradoN numero incremento = ((+ incremento) . (^2)) numero
+
+-- Definir una función esResultadoPar/2, que invocándola con número n y otro m, devuelve true si el resultado de elevar n a m es par. 
+-- Main> esResultadoPar 2 5 
+-- True 
+-- Main> esResultadoPar 3 2
+-- False 
+-- Nota Obvia: Resolverlo utilizando aplicación parcial y composición.
+
+esResultadoPar :: Number -> Number -> Bool
+esResultadoPar numero potencia = (even . (^potencia)) numero
